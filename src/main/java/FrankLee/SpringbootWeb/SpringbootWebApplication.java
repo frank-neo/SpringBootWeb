04c6e4c13cd779df4d17f3184451d48615f45f90
839908968@qq.com <839908968@qq.com>
2018-03-04 00:11:34 +0800

    first commit

---
+++ src/main/java/FrankLee/SpringbootWeb/SpringbootWebApplication.java
@@ -0,0 +1,13 @@
+package FrankLee.SpringbootWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebApplication {
+	//入口
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebApplication.class, args);
+	}
+}
